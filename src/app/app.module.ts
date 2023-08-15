3ad4bd9acda551066329ac2655992169ef867bba
Kweku Gyimah-Turkson <kweku.gyimah-turkson@amalitech.com>
2023-08-15 09:38:00 +0000

    resolved build issues relating to component paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './landing-page/side-bar/side-bar.component';
-import { SearchBarComponent } from './landing-page/search-bar/search-bar.component';
-import { SideBarButtonComponent } from './landing-page/side-bar-button/side-bar-button.component';
-import { ButtonComponent } from './login/button/button.component';
-import { FormComponent } from './login/form/form.component';
+import { SideBarComponent } from './components/landing-page/side-bar/side-bar.component';
+import { SearchBarComponent } from './components/landing-page/search-bar/search-bar.component';
+import { SideBarButtonComponent } from './components/landing-page/side-bar-button/side-bar-button.component';
+import { ButtonComponent } from './components/login/button/button.component';
+import { FormComponent } from './components/login/form/form.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
 import { HomeComponent } from './pages/landing-page/home/home.component';
