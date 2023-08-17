0f80ed4cab29c4207e483e9ce73755793ab167ac
Kweku Gyimah-Turkson <kweku.gyimah-turkson@amalitech.com>
2023-08-17 11:04:03 +0000

    make bookmark icon clickable and change on click

--- src/app/components/landing-page/carousel/carousel.component.ts
+++ src/app/components/landing-page/carousel/carousel.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit{
+@Input() bookmarkStatus?: boolean = true;
 
+ngOnInit(): void {
+    
+}
+changeState() {
+  this.bookmarkStatus = !this.bookmarkStatus;
+  console.log('clicked');
+}
 }
