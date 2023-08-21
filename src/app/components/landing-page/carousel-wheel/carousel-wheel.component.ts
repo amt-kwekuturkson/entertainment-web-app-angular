eefbaca60a79743161d0f7376fc43454ae35ff52
Kweku Gyimah-Turkson <kweku.gyimah-turkson@amalitech.com>
2023-08-21 10:49:14 +0000

    call media in movie using media service

--- src/app/components/landing-page/carousel-wheel/carousel-wheel.component.ts
+++ src/app/components/landing-page/carousel-wheel/carousel-wheel.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { MediaData } from 'src/app/types/interface';
 import { MEDIA } from 'src/app/media';
+import { mediaService } from '../../../services/media.service';
 @Component({
   selector: 'carousel-wheel',
   templateUrl: './carousel-wheel.component.html',
   styleUrls: ['./carousel-wheel.component.css']
 })
-export class CarouselWheelComponent {
+export class CarouselWheelComponent implements OnInit{
   
-mediaFiles: MediaData[] = MEDIA;  
+mediaFiles: MediaData[] = [];
+
+constructor(private mediaService: mediaService){}
+
+ngOnInit(): void {
+this.mediaFiles = this.mediaService.getMedia();
+}
 
 }
