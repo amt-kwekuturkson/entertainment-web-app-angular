588ad6c79e6a0b44a43836f3c901e728dd2780b4
Kweku Gyimah-Turkson <kweku.gyimah-turkson@amalitech.com>
2023-08-21 10:41:36 +0000

    create a service for the MEDIA json

--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -2,16 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { MediaData } from '../types/interface';
-
+import { MEDIA } from '../media';
+@Injectable({
+  providedIn: 'root'
+})
 export class mediaService {
-    private mediaURL = '';
-  private _autoReflesh$: Subject<void> = new Subject<void>();
-  constructor(private http: HttpClient) {}
-  get getReflesh$() {
-    return this._autoReflesh$;
-  }
 
-  getMedia() {
-    return this.http.get<MediaData>(this.mediaURL);
+  constructor(){}
+
+  getMedia(): MediaData[] {
+    return MEDIA;
   }
 }
